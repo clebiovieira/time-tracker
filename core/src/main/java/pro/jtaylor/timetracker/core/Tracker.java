0876f1fe95de83c620f0b929acb65f7b60094fef
Clebio Vieira <clebiovieira@users.noreply.github.com>
2017-08-02 23:51:51 -0300

    Update Tracker.java

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -9,35 +9,32 @@ import java.util.List;
 @Component
 public class Tracker {
     @Autowired
-    private List<TimeEntry> entries;
-    
+    private List<TimeEntry> entries;    
     /**
     teste
-    **/
+    */
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
     /**
     teste
-    **/
+    */
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
     /**
     teste
-    **/
+    */
     public int size() {
         return entries.size();
     }
     /**
     teste
-    **/
+    */
     public TimeEntry get(int index) {
         boolean valid = false;
         if(valid == true){
-        }
-        
+        }      
         return entries.get(index);
     }
 }
-
